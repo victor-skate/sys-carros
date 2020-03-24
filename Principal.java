265e87231db0130697e4e373122dff9d465c2343
victor moraes <victorhardflipmoraes@outlook.com>
2020-03-24 20:41:45 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,39 +1,67 @@
-package br;
 
-public class Principal {
 
+public class Principal{
+	static Carro[] lista = new Carro[10];
 	
-	public static void main(String[] args) {	
-		Carro[] lista = new Carro[10];
-		while(true) {
-			int op = Teclado.lerInt("(1)Criar (2)Consultar (3)Sair");
-			switch (op) {
+		public static void cadastrar(){
+			String marca = Teclado.lerString("digite a marca:");
+			String placa = Teclado.lerString("digite a placa:");
+			Carro carro = new Carro(marca,placa);
+			for (int i = 0; i < lista.length; i++){
+				if(lista[i]==null){
+					lista[i]=carro;
+					break;
+				}
+			}
+		System.out.println("cadastrado com sucesso");
+		menu();
+		}
+		
+		public static void consultar(){
+			for (int i = 0; i < lista.length; i++){
+				if(lista[i]!=null){
+					System.out.println(lista[i].verInfo());
+				}
+			}
+		menu();
+		}
+		
+		public static void acelerar(){
+			String placa = Teclado.lerString("placa:");
+			for (int i = 0; i < lista.length; i++){
+				if(lista[i]!=null && lista[i].getPlaca()==placa){
+					System.out.println("www");
+					lista[i].acelerar();
+				}
+			}
+		System.out.println("rasgando o asfalto");	
+		menu();
+		}
+		
+		public static void main (String[]args){
+			menu();
+			
+		}
+
+		private static void menu() {
+			int op = Teclado.lerInt("(1)cadastrar (2)consultar (3)acelerar (4)sair");
+			switch (op){
 			case 1:
-				String marca = Teclado.lerString("Digite a marca:");
-				String placa = Teclado.lerString("Digite a placa:");
-				Carro c = new Carro(marca, placa);
-				for (int i = 0; i < lista.length; i++) {
-					if(lista[i]==null) {
-						lista[i] = c;
-						break;
-					}
-				}
+				cadastrar();
 				break;
+			
 			case 2:
-				for (int i = 0; i < lista.length; i++) {
-					if(lista[i]!=null) {
-						System.out.println(lista[i].verInfo());
-					}
-				}
+				consultar();
 				break;
+			
 			case 3:
-				System.exit(0);
+				acelerar();
 				break;
-			default:
+				
+			case 4:
+				System.exit(0);
 				break;
 			}
 			
 		}
-		
-	}
 }
